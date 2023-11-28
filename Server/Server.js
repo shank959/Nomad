c490c1a6623ed2d4741ff312dd7fcc40ac9bb046
meena-sal <meenasalib2004@gmail.com>
2023-11-27 23:41:53 -0800

    Move backend setup code to Server.js

--- Server/Server.js
+++ Server/Server.js
@@ -1,6 +1,18 @@
 const { MongoClient, ServerApiVersion } = require('mongodb')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
 
 const uri ="mongodb+srv://Nomad:ExploreLA123@clusternomad.l4pqavm.mongodb.net/?retryWrites=true&w=majority";
+
+app.listen(PORT, () =>{
+    console.log('Server is running!');
+  })
 
 async function main(){
     //create client object
