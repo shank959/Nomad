b39b2a27333d3a2e24d1ed229a224d5e1d0ff921
MatthewChuang <matthewchuang04@gmail.com>
2023-11-27 23:16:52 -0800

    Basic Backend/Creating a newUser Schema

--- Server/userModel.js
+++ Server/userModel.js
@@ -1,20 +1,8 @@
 //defining the structure of our model
 const mongoose = require('mongoose');
 
-//implement Schema: This is a class that allows me to 
-//define the structure of documents in database
-const UsersSchema = new mongoose.Schema({
-    email: String,
-    username: String,
-    password: String,
-});
-
-const UsersModel = mongoose.model('Users', UsersSchema, 'user_login')
-module.exports = UsersModel;
-
 //to update API routes
 //this directs API/HTTP requests to backend resources
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -23,3 +11,25 @@ const bodyParser = require('body-parser');
 const app = express();
 
 //setup port number - this is where we tell where the app to listen in
+const PORt = process.env.PORT || 30000;
+
+//implement Schema: This is a class that allows me to 
+//define the structure of documents in database
+const UsersSchema = new mongoose.Schema({
+    email: {type: String, required:true},
+    username: {type: String, required:true},
+    password: {type: String, required:true}
+});
+
+const UsersModel = mongoose.model('Users', UsersSchema, 'user_login')
+app.use(bodyParser.json());
+
+//create endpoint: Creating a new user
+app.post('/createusers', async (req, res) => {
+    try {
+        const {email, username, password } = req.body;
+        }
+    catch (error){
+        console.error(error);
+    }
+    });
