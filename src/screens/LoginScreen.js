d6ef82b61948346bd662a70dbbcac72dac538653
AyaanRaina <ayaanrainaa@gmail.com>
2023-12-02 15:56:13 -0800

    forgot password button

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, TextInput, TouchableOpacity } from "react-native";
-import { Image } from "react-native";
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 
 export default function LoginScreen({ navigation }) {
   const [username, setUsername] = useState("");
@@ -8,18 +7,22 @@ export default function LoginScreen({ navigation }) {
 
   const handleLogin = () => {
     // Add your login logic here
-    // username === "user" && password === "password"
     if (true) {
       // Successful login, navigate to the next screen
-      navigation.navigate("MainTabScreen", { screen: "MapScreen", });
+      navigation.navigate("MainTabScreen", { screen: "MapScreen" });
     } else {
       // Display an error message or handle unsuccessful login
       alert("Invalid credentials. Please try again.");
     }
   };
+
   const createAccount = () => {
     navigation.navigate("CreateAccountScreen");
+  };
 
+  const handleForgotPassword = () => {
+    // Placeholder for forgot password functionality
+    alert("Forgot Password functionality not implemented yet.");
   };
 
   return (
@@ -46,6 +49,9 @@ export default function LoginScreen({ navigation }) {
       <TouchableOpacity style={styles.button} onPress={createAccount}>
         <Text style={styles.buttonText}>Create Account</Text>
       </TouchableOpacity>
+      <TouchableOpacity style={[styles.button, styles.forgotPasswordButton]} onPress={handleForgotPassword}>
+        <Text style={styles.forgotPasswordButtonText}>Forgot Password?</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -61,11 +67,6 @@ const styles = StyleSheet.create({
     width: 230,
     height: 230,
     marginBottom: 10,
-  },
-  title: {
-    fontSize: 35,
-    marginBottom: 20,
-    color: "#ffffff",
   },
   input: {
     width: 300,
@@ -91,6 +92,15 @@ const styles = StyleSheet.create({
   },
   loginButton: {
     marginTop: 35, 
+  },
+  forgotPasswordButton: {
+    width: 130, // Smaller width for the forgot password button
+    height: 25,
+  },
+  forgotPasswordButtonText: {
+    color: "white",
+    fontSize: 12, // Smaller font size for the forgot password button text
+    fontWeight: 400,
   },
   buttonText: {
     color: "white",
