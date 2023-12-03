8c3c3f73bb0e6170628a64f6004254b3b17c7841
MatthewChuang <matthewchuang04@gmail.com>
2023-12-03 01:46:00 -0800

    Organized structure of Server.js, routes, and models

---
+++ Server/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+//this lets us parse incoming request bodies in JSON format (this is like a middleman)
+app.use(bodyParser.json());
+
+// User Schema
+const UsersSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+// Hash password before saving
+UsersSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+// User Model
+const UsersModel = mongoose.model('user_login', UsersSchema);
+
+module.exports = UsersModel;
