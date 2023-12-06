840850d949d8401cef265604f90646c74783d999
shank959 <shanvkunzru@gmail.com>
2023-12-05 22:04:14 -0800

    fixed error in last commit

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ import LoginScreen from './src/screens/LoginScreen';
 import MainTabScreen from './src/navigation/MainTabScreen';
 import CreateAccountScreen from './src/screens/CreateAccountScreen';
 import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
-import CreatePostScreen from './src/screens/CreatePostScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -22,7 +21,6 @@ export default function App() {
         <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="CreateAccountScreen" component={CreateAccountScreen} options={{ headerShown: false }} />
         <Stack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="CreatePostScreen" component={CreatePostScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
