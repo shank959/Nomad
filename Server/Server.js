d8ba59ebf1ab7e94034604b1aa0b5e4876600399
meena-sal <meenasalib2004@gmail.com>
2023-12-05 21:58:12 -0800

    Setup simulator and correct dependencies

--- Server/Server.js
+++ Server/Server.js
@@ -64,7 +64,10 @@ app.post('/posts', (req, res) => {
 
 
 
-
+app.post('/test', (req, res) => {
+    console.log('Received data:', req.body);
+    res.status(200).json({ message: 'Data received successfully!' });
+  });
 
 
 
